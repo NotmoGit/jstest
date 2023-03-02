747b6d166e5277c77df91dda1965ff3260c70974
NotmoGit <11445541+NotmoGit@users.noreply.github.com>
2023-03-02 15:04:48 +0000

    creating a rock paper scissor game  plus UI

--- js-rockpaper.js
+++ js-rockpaper.js
@@ -5,51 +5,78 @@ function getComputerChoice () {
     computerChoice = choices.at(Math.floor(Math.random()*3))
     return computerChoice
 }
-function getPlayerChoice () {
-    playerChoice = prompt("Choose: Rock, Paper or Scissors?")
-    choiceLowerCase = playerChoice.toLowerCase()
-    choiceCap = choiceLowerCase.slice(0,1).toUpperCase()
-    choiceSlice = choiceLowerCase.slice(1)
-    choiceOutput = choiceCap + choiceSlice
-    allowed = ["Rock","Paper", "Scissors"]
-    if (allowed.includes(choiceOutput)) {
-        return choiceOutput
-    } else {
-        return getPlayerChoice()
-    }
-}
 
-function resolveGame (compChoice,playerChoice) {
+function resolveGame (playerChoice) {
+    
+    compChoice = getComputerChoice()
+    
     if (compChoice == playerChoice) {
-        return "Game is a draw!"
+        result = "Game is a draw!"
     } else if (compChoice == "Rock" && playerChoice == "Paper"){
-        return "Computer Wins"
+        result = "Computer Wins"   
     } else if (compChoice == "Rock" && playerChoice == "Scissors"){
-        return "Computer Wins"
+        result = "Computer Wins"  
     } else if (compChoice == "Scissors" && playerChoice == "Paper"){
-        return "Computer Wins"
+        result = "Computer Wins"
     } else {
-        return "You Win!"
+        result = "You Win!"
     }
+    return result
+    
+    
 }
 
-function game (rounds) {
-    let playerScore = 0
-    let compScore = 0
-    for (let i = 1; i <= rounds; i++) {
-        const finalPlayerChoice = getPlayerChoice()
-    const finalCompChoice = getComputerChoice()
-    result = resolveGame(finalCompChoice,finalPlayerChoice)
-        if (result == "Computer Wins") {
-            compScore++
-        } else if (result == "You Win"){
-            playerScore++
+function finalScore(player,computer){
+    if (player > computer) {
+        finalResult = "You Win"
+    } else {
+        finalResult = "Computer Wins"
+    }
+    return finalResult
+
+}
+
+const buttons = document.querySelectorAll("button")
+const resultDiv = document.querySelector('#results')
+const scoreDiv = document.querySelector('#score')
+const roundDiv = document.querySelector('#rounds')
+let round = 0
+let playerScore = 0
+let compScore = 0
+
+score = "Player Score = " + playerScore + ", Computer Score = " + compScore
+
+buttons.forEach((button => {
+    button.addEventListener('click', () => {
+        playerButton = button.innerHTML
+        if (round <= 4) {
+            resolveGame(playerButton)
+            
+            if (result == "You Win!") {
+                playerScore++
+            } else if (result == "Game is a draw!") {
+                playerScore++
+                compScore++
+            } else {
+                compScore++
+            }
+            score = "Player Score = " + playerScore + ", Computer Score = " + compScore
+            resultDiv.textContent = result
+            scoreDiv.textContent = score
+            round++
+            roundDiv.textContent = round
+        } else {
+            finalScore(playerScore,compScore)
+            resultDiv.textContent = 
+            " Your score was " + playerScore + ", Computer score was " + compScore + "  - therefore " + finalResult
+            round = 0
+            playerScore = 0
+            compScore = 0
+            scoreDiv.textContent = ""
+            roundDiv.textContent = ""
         }
-
-    console.log("Round " + i + ": Computer said " + finalCompChoice + ", you said " + finalPlayerChoice + " so: " + result)
-    }
-    console.log("Final Score was " + playerScore + ":" + compScore + ". Please play again")
-}
+    })
+}))
 
 
 
